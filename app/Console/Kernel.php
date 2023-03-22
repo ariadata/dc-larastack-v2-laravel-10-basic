d563fe96680ca32d48296d4b9708dcdeb6a54e21
Mehrdad Amini <pcmehrdad@gmail.com>
2023-03-22 18:13:20 +0300

    add laravel-short-schedule

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -9,16 +9,33 @@ class Kernel extends ConsoleKernel
 {
     /**
      * Define the application's command schedule.
+     *
+     * @param  \Illuminate\Console\Scheduling\Schedule  $schedule
+     * @return void
      */
-    protected function schedule(Schedule $schedule): void
+    protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
     }
 
     /**
-     * Register the commands for the application.
+     * Define the application's command Short schedules.
+     * https://github.com/spatie/laravel-short-schedule
+     *
+     * @param  \Spatie\ShortSchedule\ShortSchedule  $shortSchedule
+     * @return void
      */
-    protected function commands(): void
+    protected function shortSchedule(\Spatie\ShortSchedule\ShortSchedule $shortSchedule)
+    {
+        // $shortSchedule->command('short-command')->everySeconds(2);
+    }
+
+    /**
+     * Register the commands for the application.
+     *
+     * @return void
+     */
+    protected function commands()
     {
         $this->load(__DIR__.'/Commands');
 
